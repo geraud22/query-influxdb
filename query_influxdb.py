2991ae78de582195423ff8dd2f4108e5744c0354
Geraud Greyling <96425734+geraud22@users.noreply.github.com>
2024-04-29 12:07:21 +0200

    Added flask-cors for development environment.

--- query_influxdb.py
+++ query_influxdb.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 import requests
 from dotenv import load_dotenv
 import os
@@ -13,6 +14,7 @@ INFLUX_API_TOKEN = os.getenv("INFLUX_API_TOKEN")
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/query', methods=['GET'])
 def query_influxdb():
