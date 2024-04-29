94265a85980c64025b82c9734e1f825f2a472e77
Geraud Greyling <96425734+geraud22@users.noreply.github.com>
2024-04-29 13:20:50 +0200

    Containerizing the API.

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use the official Python image from Docker Hub
+FROM python:latest
+
+# Update the package lists
+RUN apt-get update
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install any needed dependencies specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Make port 5000 available to the network
+EXPOSE 5000
+
+# Set the FLASK_APP environment variable
+ENV FLASK_APP=query_influxdb.py
+
+# Run the Flask application
+CMD ["flask", "run", "--host=0.0.0.0"]
--- query_influxdb.py
+++ query_influxdb.py
@@ -1,12 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 import requests
-from dotenv import load_dotenv
-import os
 from csv2json import csv_to_json
-
-load_dotenv()
-INFLUX_URL = os.getenv("INFLUX_URL")
 
 app = Flask(__name__)
 CORS(app)
@@ -15,20 +10,21 @@ CORS(app)
 def query_influxdb():
     # Define endpoint parameters
     parameters = {
-    "organisation": request.args.get('organisation'),
-    "auth_token": request.args.get('token'),
-    "bucket": request.args.get('bucket'),
-    "app_id": request.args.get('app-id'),
-    "device_id": request.args.get('device-id'),
-    "field": request.args.get('field'),
-    "range": request.args.get("range"),
+        "url": request.args.get('host-url'),    
+        "organisation": request.args.get('organisation'),
+        "auth_token": request.args.get('token'),
+        "bucket": request.args.get('bucket'),
+        "app_id": request.args.get('app-id'),
+        "device_id": request.args.get('device-id'),
+        "field": request.args.get('field'),
+        "range": request.args.get("range"),
     }
     
     for key,value in parameters.items():
         if value is None:
             return {'error': 'Route received insufficient arguments.'}
     
-    url = f"{INFLUX_URL}/api/v2/query?org={parameters['organisation']}"
+    url = f"{parameters['url']}/api/v2/query?org={parameters['organisation']}"
     headers = {
         'Content-Type': 'application/vnd.flux',
         'Accept': 'application/csv',
@@ -46,6 +42,3 @@ def query_influxdb():
         return jsonArray
     else:
         return {'error': f'Failed to query InfluxDB. Status code: {response.status_code}. Reason: {response.reason}'}
-    
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
