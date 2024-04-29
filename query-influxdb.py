51cef861bdee2a8825204caeed6804f5fa65605e
Geraud Greyling <96425734+geraud22@users.noreply.github.com>
2024-04-29 11:27:30 +0200

    parse-csv-json name changed to csv2json. New module - fluxquerycreator. query-influxdb updated to use new modules.

--- query-influxdb.py
+++ query-influxdb.py
@@ -1,13 +1,14 @@
 import requests
 from dotenv import load_dotenv
 import os
+from csv2json import csv_to_json
+from fluxquerycreator import createFluxQuery
 
 load_dotenv()
 
 INFLUX_URL = os.getenv("INFLUX_URL")
 INFLUX_ORG = os.getenv("INFLUX_ORG")
 INFLUX_API_TOKEN = os.getenv("INFLUX_API_TOKEN")
-INFLUX_QUERY = os.getenv("INFLUX_QUERY")
 
 def query_influxdb():
     url = f"{INFLUX_URL}/api/v2/query?org={INFLUX_ORG}"
@@ -16,20 +17,17 @@ def query_influxdb():
         'Accept': 'application/csv',
         'Authorization': f'Token {INFLUX_API_TOKEN}'
     }
-    data = INFLUX_QUERY
+    data = createFluxQuery()
 
     response = requests.post(url, headers=headers, data=data)
     
     if response.status_code == 200:
-        with open('query-data.csv', 'w') as csv_file:
-            csv_file.write(response.text)
+        csv_to_json(response.text, 'query-data.json')
         return True
     else:
-        print(f"Failure Code: {response.status_code}")
+        print(f"Failure Code: {response.status_code} : {response.text}")
         return False
     
 if __name__ == "__main__":
     if query_influxdb():
-        print(f"CSV data saved to query-data.csv")
-    else:
-        print("Failed to query InfluxDB")
+        print(f"CSV data saved to query-data.json")
